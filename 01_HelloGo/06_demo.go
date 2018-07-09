6f16beeac04eea42070be9c2c7aab2edf823b7fe
Godlovesmile <2272674323@qq.com>
2018-07-09 10:17:48 +0800

    update

--- 01_HelloGo/06_demo.go
+++ 01_HelloGo/06_demo.go
@@ -1,19 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-)
+// import (
+// 	"fmt"
+// 	"runtime"
+// )
 
-// 1. goroutine
-func say(s string) {
-	for i := 0; i < 5; i++ {
-		runtime.Gosched()
-		fmt.Println(s)
-	}
-}
+// // 1. goroutine
+// func say(s string) {
+// 	for i := 0; i < 5; i++ {
+// 		runtime.Gosched()
+// 		fmt.Println(s)
+// 	}
+// }
 
-func main() {
-	go say("world") // 开启一个新的 Goroutines 执行
-	say("hello")    // 当前 Goroutines 执行
-}
+// func main() {
+// 	go say("world") // 开启一个新的 Goroutines 执行
+// 	say("hello")    // 当前 Goroutines 执行
+// }
