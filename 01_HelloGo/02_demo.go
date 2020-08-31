f397e6cdd109686404cc2b6e1684f46650a11680
hortor <>
2020-08-31 10:29:38 +0800

    2020-8-31

--- 01_HelloGo/02_demo.go
+++ 01_HelloGo/02_demo.go
@@ -1,7 +1,5 @@
 package main
 
-// import "fmt"
-
 // type testInt func(int) bool // 声明一个函数类型
 
 // func isOdd(i int) bool {
@@ -24,6 +22,14 @@ package main
 
 // func main() {
 // 	slice := []int{1, 2, 3, 4, 5}
-// 	odd := filter(slice, isOdd)
-// 	fmt.Println("Odd elements are: ", odd)
+// 	// odd := filter(slice, isOdd)
+// 	fmt.Println("Odd elements are: ", slice)
+
+// 	for _, v := range slice {
+// 		fmt.Println("--->>>", v)
+// 	}
+
+// 	for i := 0; i < 5; i++ {
+// 		defer fmt.Println("--->>>", i)
+// 	}
 // }
