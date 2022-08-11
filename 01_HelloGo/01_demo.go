16bfa657d21828498fa0574e56c5a8f894296925
lizhi <15501251520@163.com>
2022-08-11 23:32:38 +0800

    feat: go 基础更新

--- 01_HelloGo/01_demo.go
+++ 01_HelloGo/01_demo.go
@@ -1,9 +1,12 @@
 package main
 
-// func main() {
-// 	fmt.Printf("Hello, world or 你好世界\n")
+import "fmt"
 
-// 	s := "hello"
-// 	s = "c" + s[1:]
-// 	fmt.Printf("%s\n", s)
-// }
+func main() {
+	fmt.Printf("Hello, world or 你好世界\n")
+
+	s := "hello"
+	s = "c" + s[1:]
+	fmt.Printf("%s\n", s)
+	fmt.Printf("type of s = %T", s)
+}
